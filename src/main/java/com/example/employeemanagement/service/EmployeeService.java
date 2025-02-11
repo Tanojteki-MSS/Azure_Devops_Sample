1c00020d81795fa0149e764cd48126559e2d64d2
Tanojteki-MSS <109952429+Tanojteki-MSS@users.noreply.github.com>
2025-02-11 17:01:47 +0530

    Create EmployeeService.java

---
+++ src/main/java/com/example/employeemanagement/service/EmployeeService.java
@@ -0,0 +1,6 @@
+package com.example.employeemanagement.service;
+
+import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
